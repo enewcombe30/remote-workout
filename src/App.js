194e04acb043975c4dbec221fa108c6d29b59fe9
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-05-23 08:38:05 +0100

    Basic style updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div className="text-6xl text-red-500 w-fit mx-auto">
-        Setting up Tailwindcss
+    <div className="max-h-screen h-fit">
+      <div className="my-[15%] h-fit">
+        <div className="text-6xl text-red-500 w-fit mx-auto">
+          Setting up Remote Workout App
+        </div>
+        <div className="w-fit mx-auto my-4 text-3xl">
+          Exercise and increase productivity with low to medium intensity
+          workouts while working remotely
+        </div>
+        <div className="w-fit mx-auto">
+          Check the link below for more info about taking breaks and
+          productivity
+        </div>
+        <div className="w-fit mx-auto text-red-500">
+          <a href="https://weworkremotely.com/science-says-you-have-to-stop-taking-breaks-wrong-to-be-more-productive">
+            Link Here!!
+          </a>
+        </div>
       </div>
-      <div className="w-fit mx-auto">Now connected to Github</div>
     </div>
   );
 }
